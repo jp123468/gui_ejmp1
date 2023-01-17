24b69f6ca2de01f82f8e0e109032b043d94fee30
POO <asada@hotmail.com>
2023-01-17 11:00:17 -0500

    Uso form

--- src/v1.java
+++ src/v1.java
@@ -1,8 +1,17 @@
 import javax.swing.*;
 
 public class v1 {
+    private JPanel panel;
     private JTextField ingreseSuNombreTextField;
     private JButton OKButton;
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame("v1");
+        frame.setContentPane(new v1().panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 
 
 }
