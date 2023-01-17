a60fa6690a6f606a1d93a63d084750ec64e22ed3
POO <asada@hotmail.com>
2023-01-17 11:59:17 -0500

    boton con acciones

--- src/v1.java
+++ src/v1.java
@@ -1,17 +1,35 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class v1 {
-    private JPanel panel;
-    private JTextField ingreseSuNombreTextField;
-    private JButton OKButton;
+    private JPanel panel1;
+    private JButton IMCButton;
+    private JTextField txtFieldAltura;
+    private JTextField textFieldPeso;
+    private JLabel LabelPeso;
+    private JLabel LabelAltura;
+    private JLabel LabelREspuesta;
 
-    public static void main(String[] args){
-        JFrame frame = new JFrame("v1");
-        frame.setContentPane(new v1().panel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+
+    public v1() {
+        IMCButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //LabelREspuesta.getText()
+                LabelREspuesta.setText("HOLA");
+            }
+        });
     }
 
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("v1");
+        frame.setContentPane((new v1()).panel1);
+        frame.setDefaultCloseOperation(3);
+        frame.pack();
+        frame.setVisible(true);
+
+
+    }
 
 }
